8dc177ef1b66f5c5f5b7ca426e8929345f19d891
Dustin Frisch <fooker@lab.sh>
2015-04-01 23:25:51 +0200

    BATMAN-ADV version bump

--- site.mk
+++ site.mk
@@ -1,5 +1,5 @@
 GLUON_SITE_PACKAGES := \
-gluon-mesh-batman-adv-14 \
+gluon-mesh-batman-adv-15 \
 gluon-alfred \
 gluon-announced \
 gluon-autoupdater \
@@ -23,7 +23,7 @@ iptables \
 haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.4.1+0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.5.0+0-exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
