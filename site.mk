f265917e23851b4655ffb3095a1e12207a7fde08
Ronny Trommer <ronny@opennms.org>
2015-08-10 20:16:44 +0200

    Migration gluon packages und entfernen des mirrors fuer development branch um auto updates zu verhindern.
    Mirror auf leeres array geändert
    
    Formatting pro mode
    
    Removed gw01 and gw02
    
    Revert "Removed gw01 and gw02"
    
    This reverts commit 4008bd8febd863e21ae716b66e42bc1482fd97ed.
    
    changes for test purposes
    
    use development branch
    
    Set to latest gluon-fffd-autokey commit

--- site.mk
+++ site.mk
@@ -1,30 +1,31 @@
 GLUON_SITE_PACKAGES := \
-gluon-mesh-batman-adv-15 \
-gluon-alfred \
-gluon-announced \
-gluon-autoupdater \
-gluon-config-mode-hostname \
-gluon-config-mode-autoupdater \
-gluon-config-mode-mesh-vpn \
-gluon-config-mode-geo-location \
-gluon-config-mode-contact-info \
-gluon-ebtables-filter-multicast \
-gluon-ebtables-filter-ra-dhcp \
-gluon-luci-admin \
-gluon-luci-autoupdater \
-gluon-luci-portconfig \
-gluon-luci-private-wifi \
-gluon-next-node \
-gluon-mesh-vpn-fastd \
-gluon-fffd-autokey \
-gluon-radvd \
-gluon-status-page \
-iwinfo \
-iptables \
-haveged
+  gluon-mesh-batman-adv-15 \
+  gluon-alfred \
+  gluon-announced \
+  gluon-autoupdater \
+  gluon-config-mode-autoupdater \
+  gluon-config-mode-contact-info \
+  gluon-config-mode-core \
+  gluon-config-mode-geo-location \
+  gluon-config-mode-hostname \
+  gluon-config-mode-mesh-vpn \
+  gluon-ebtables-filter-multicast \
+  gluon-ebtables-filter-ra-dhcp \
+  gluon-fffd-autokey \
+  gluon-luci-admin \
+  gluon-luci-autoupdater \
+  gluon-luci-portconfig \
+  gluon-luci-private-wifi \
+  gluon-next-node \
+  gluon-mesh-vpn-fastd \
+  gluon-radvd \
+  gluon-setup-mode \
+  gluon-status-page \
+  iwinfo \
+  iptables \
+  haveged
 
-
-DEFAULT_GLUON_RELEASE := development-snapshot-$(shell date '+%Y%m%d%H%M%S')
+DEFAULT_GLUON_RELEASE := 2015.1.2-dev-snapshot-$(shell date '+%Y%m%d%H%M%S')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
