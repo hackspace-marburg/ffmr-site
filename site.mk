f0cff2a5942b071f750b26dea3ab60fda2ac4c20
Oleander Reis <oleander@oleander.cc>
2015-11-29 00:43:53 +0100

    initial site configuration for marburg

--- site.mk
+++ site.mk
@@ -1,38 +1,37 @@
 GLUON_SITE_PACKAGES := \
-  gluon-mesh-batman-adv-15 \
-  gluon-alfred \
-  gluon-announced \
-  gluon-autoupdater \
-  gluon-config-mode-autoupdater \
-  gluon-config-mode-contact-info \
-  gluon-config-mode-core \
-  gluon-config-mode-geo-location \
-  gluon-config-mode-hostname \
-  gluon-config-mode-mesh-vpn \
-  gluon-ebtables-filter-multicast \
-  gluon-ebtables-filter-ra-dhcp \
-  gluon-fffd-autokey \
-  gluon-fffd-wifi-aliases \
-  gluon-luci-theme \
-  gluon-luci-admin \
-  gluon-luci-autoupdater \
-  gluon-luci-portconfig \
-  gluon-luci-private-wifi \
-  gluon-luci-mesh-vpn-fastd \
-  gluon-next-node \
-  gluon-mesh-vpn-fastd \
-  gluon-radvd \
-  gluon-setup-mode \
-  gluon-status-page \
-  iwinfo \
-  iptables \
-  haveged
+    gluon-mesh-batman-adv-15 \
+    gluon-alfred \
+    gluon-announced \
+    gluon-autoupdater \
+    gluon-config-mode-autoupdater \
+    gluon-config-mode-contact-info \
+    gluon-config-mode-core \
+    gluon-config-mode-geo-location \
+    gluon-config-mode-hostname \
+    gluon-config-mode-mesh-vpn \
+    gluon-ebtables-filter-multicast \
+    gluon-ebtables-filter-ra-dhcp \
+    gluon-luci-theme \
+    gluon-luci-admin \
+    gluon-luci-autoupdater \
+    gluon-luci-portconfig \
+    gluon-luci-wifi-config \
+    gluon-luci-mesh-vpn-fastd \
+    gluon-next-node \
+    gluon-mesh-vpn-fastd \
+    gluon-radvd \
+    gluon-setup-mode \
+    gluon-status-page \
+    iwinfo \
+    iptables \
+    haveged
+
+DEFAULT_GLUON_RELEASE := snapshot-$(shell date '+%Y%m%d%H%M%S')
+DEFAULT_GLUON_PRIORITY := 0
+
+# languages to include in images
+GLUON_LANGS ?= en de
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= snapshot-$(shell date '+%Y%m%d%H%M%S')
-
-# Default priority for updates.
-GLUON_PRIORITY ?= 0
-
-# Languages to include
-GLUON_LANGS ?= en de
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_PRIORITY ?= ${DEFAULT_GLUON_PRIORITY}
